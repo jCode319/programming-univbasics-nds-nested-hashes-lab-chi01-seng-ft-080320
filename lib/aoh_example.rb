c333bd2c47bf8aa1405df8761d7ee71fffd7d904
Jarrell Jackson <jdjack3@gmail.com>
2020-07-20 01:36:01 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -7,8 +7,9 @@ KATE_G = { name:  "Kate Gompert", occupation: "None" }
 BRUCE_G = { name:  "Bruce Green", occupation: "Fan of Mildred" }
 
 def assembled_aoh
-  Hash = [
+  hashes = [
     DON_G,
+    JOELLE_VD,
     PAT_M,
     KATE_G,
     BRUCE_G
